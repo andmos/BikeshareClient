9efac7a457a34579cbc307bd25211dc866bc71c5
andmos <andreas.mosti@gmail.com>
2019-04-24 16:59:55 +0200

    mapping bool integers to bool fixes #34

---
+++ src/BikeshareClient/BikeshareClient/Helpers/IntegerToBoolConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace BikeshareClient.Helpers
+{
+    public class IntegerToBoolConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(bool);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            return reader.Value.ToString() == "1";
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            writer.WriteValue(((bool)value) ? 1 : 0);
+        }
+    }
+}
---
+++ src/BikeshareClient/TestBikeshareClient/Helpers/TestIntegerToBoolConverter.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using BikeshareClient.Helpers;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace TestBikeshareClient.Helpers
+{
+    public class TestIntegerToBoolConverter
+    {
+        [Fact]
+        public void ReadJson_GivenJsonWithTrueInteger_DeserializesToTrue() 
+        {
+            var jsonString = "{ boolInt : 1 }";
+
+            var testObject = JsonConvert.DeserializeObject<TestObject>(jsonString);
+
+            Assert.True(testObject.boolInt);
+        }
+
+        [Fact]
+        public void ReadJson_GivenJsonWithFalseInteger_DeserializesToFalse()
+        {
+            var jsonString = "{ boolInt : 0 }";
+
+            var testObject = JsonConvert.DeserializeObject<TestObject>(jsonString);
+
+            Assert.False(testObject.boolInt);
+        }
+
+        [Fact]
+        public void ReadJson_GivenJsonWithHigherInteger_DeserializesToFalse()
+        {
+            var jsonString = "{ boolInt : 2 }";
+
+            var testObject = JsonConvert.DeserializeObject<TestObject>(jsonString);
+
+            Assert.False(testObject.boolInt);
+        }
+
+        [Fact]
+        public void ReadJson_GivenJsonWithLowerInteger_DeserializesToFalse()
+        {
+            var jsonString = "{ boolInt : -1 }";
+
+            var testObject = JsonConvert.DeserializeObject<TestObject>(jsonString);
+
+            Assert.False(testObject.boolInt);
+        }
+
+        [Fact]
+        public void WriteJson_GivenTrueBoolProperty_SerializesToHighInteger() 
+        {
+            var testObject = new TestObject { boolInt = true };
+
+            var jsonString = JsonConvert.SerializeObject(testObject);
+
+            Assert.Contains("1", jsonString);
+        }
+
+        [Fact]
+        public void WriteJson_GivenFalseBoolProperty_SerializesToLowInteger()
+        {
+            var testObject = new TestObject { boolInt = false };
+
+            var jsonString = JsonConvert.SerializeObject(testObject);
+
+            Assert.Contains("0", jsonString);
+        }
+
+        private class TestObject
+        {
+            [JsonConverter(typeof(IntegerToBoolConverter))]
+            public bool boolInt { get; set; }
+        }
+    }
+
+}
