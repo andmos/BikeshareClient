43083d8f7a8ea346922193f39538cfadbc0fc913
Andreas Mosti <1283556+andmos@users.noreply.github.com>
2022-05-05 13:44:10 +0200

    Correcting client again

--- src/BikeshareClient.DependencyInjection/BikeshareClient.DependencyInjection/ServiceCollectionExtensions.cs
+++ src/BikeshareClient.DependencyInjection/BikeshareClient.DependencyInjection/ServiceCollectionExtensions.cs
@@ -3,6 +3,10 @@ using Microsoft.Extensions.DependencyInjection;
 
 public static class ServiceCollectionExtensions
 {
-    public static IHttpClientBuilder AddBikeshareClient(this IServiceCollection services)
-        => services.AddHttpClient<IBikeshareClient>("GbfsClient", httpClient => new Client("", httpClient));
+    public static IHttpClientBuilder AddBikeshareClient(this IServiceCollection services, string baseUrl)
+        => services.AddHttpClient<IBikeshareClient>("GbfsClient", httpClient =>
+        {
+            httpClient.BaseAddress = new Uri(baseUrl);
+            new Client(string.Empty, httpClient);
+        });
 }
